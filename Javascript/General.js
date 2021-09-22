dd2177e4eda78312b35065ca7320aeddba45ad4d
MarySanchezC <marysanchezc1204@gmail.com>
2021-09-21 19:14:36 -0500

    Proyecto 1 semana 1

---
+++ Javascript/General.js
@@ -0,0 +1,39 @@
+alert("¡Welcome to my website!");
+
+/* console.log("Welcome user to Mary's website");
+
+prompt("Type your name");
+
+confirm("¿Do you want to go out of this website?"); */
+
+const album1 = () => {
+    let lol = " prueba" ;
+    alert("Esta imagen se llama"+" Chriss in album" + lol );
+    console.log();
+};
+
+const album2 = () => {
+    let nombre = alert("Esta imagen se llama"+" Chriss in draw");
+    console.log();
+};
+
+const album3 = () => {
+    let nombre = alert("Esta imagen se llama"+" Chriss in concert");
+    console.log();
+};
+
+const album4 = () => {
+    let nombre = alert("Esta imagen se llama"+" Chriss in abstrac");
+    console.log();
+};
+
+const album5 = () => {
+    let nombre = alert("Esta imagen se llama"+" Chriss singing");
+    console.log();
+};
+const album6 = () => {
+    let nombre = alert("Esta imagen se llama"+" Chriss in dibujo");
+    console.log();
+};
+
+
